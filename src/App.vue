6ec3d52679dd0697a7b68c56f0c1907769a8d3f0
zetromex <105536448+zetromex@users.noreply.github.com>
2022-08-04 14:15:55 +0430

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template >
+  <div id="app" class="w-full	h-auto">
+    <div class="w-[372px] h-[520px] bg-[#282E30] m-10 relative rounded-3xl p-5 font-Roboto">
+      <div class="w-[337px] h-[228px] rounded-xl bg-[#31383A] mb-2 relative p-10"><img class="m-auto" src="./assets/Vector.png" alt=""></div>
+      <div class="mx-auto w-[340px] mb-[10px]">
+        <h1 class="text-[30px] text-white mb-[-5px]	">Lorem ispam...</h1>
+        <h3 class=" text-[20px] opacity-80	text-[#52595B] ">lorem lorem</h3>
+      </div>
+      <div
+      class="w-[340px] text-[14px] text-[#F5F6F5] mb-[25px] ">
+        <p>orem ipsum dolor sit amet, consectetur adipiscing
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi</p>
+      </div>
+      <div class="">
+        <button class="text-[24px] text-[#2CD142] w-[166px] h-[59px] hover:text-[#ffff] hover:bg-[#42BB52] hover:rounded-[46px] hover:transition duration-500">Lorem</button>
+        <button class="text-[24px] text-[#2CD142] w-[166px] h-[59px] hover:text-[#ffff] hover:bg-[#42BB52] hover:rounded-[46px] hover:transition duration-500 ">Ispam</button>
+      </div>
+      <div class="w-[139px] h-[120px] bg-[#3B4244] absolute left-[285px] bottom-[230px] rounded-[24px] shadow-[0px 12px 20px rgb(0 0 0 / 18%)] x">
+        <button class="flex  w-[139px] h-[40px] bg-[#3B4244] rounded-t-[24px] hover:bg-[#43484A]"><img class="w-[20px] mt-[15px] ml-[20px]" src="./assets/save-icon-36515.png"><span class="ml-[20px] mt-[12px] text-[#ffff]">Save</span></button>
+        <button class="flex  w-[139px] h-[40px] bg-[#3B4244] hover:bg-[#43484A]"><img class="w-[20px] mt-[11px] ml-[20px]" src="./assets/copy.png"><span class="ml-[15px] mt-[9px] text-[#ffff]">Copy link</span></button>
+        <button class="flex  w-[139px] h-[40px] bg-[#3B4244] rounded-b-[24px] hover:bg-[#43484A]"><img class="w-[20px] mt-[11px] ml-[20px]" src="./assets/symbole-d-avertissement-rouge.png"><span class=" text-[#ffff] ml-[18px] mt-[9px]">report</span></button>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto"');
+</style>
